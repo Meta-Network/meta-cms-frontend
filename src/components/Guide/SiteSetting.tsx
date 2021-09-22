be851311cba35bea7294087e5746188e45d695fa
Casimir Crystal <casimir.crystal.blue@outlook.com>
2021-09-22 14:57:32 +0800

    fix: border of guide card

--- src/components/Guide/SiteSetting.tsx
+++ src/components/Guide/SiteSetting.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { message, Upload } from 'antd';
-import ProCard from '@ant-design/pro-card';
 import { useModel } from '@@/plugin-model/useModel';
 import { uploadAvatar } from '@/services/api/global';
 import { requestStorageToken } from '@/services/api/meta-ucenter';
@@ -77,97 +76,95 @@ export default () => {
         <p>数据会同步至 Meta Space，并在您的个人站点展示给访客。</p>
       </div>
 
-      <ProCard>
-        <ProForm
-          style={{ width: 500 }}
-          name="site-info"
-          initialValues={initialValues}
-          onFinish={handleFinishing}
-          requiredMark="optional"
+      <ProForm
+        style={{ width: 500 }}
+        name="site-info"
+        initialValues={initialValues}
+        onFinish={handleFinishing}
+        requiredMark="optional"
+      >
+        <ProFormText
+          width="md"
+          name="title"
+          label="标题"
+          placeholder="你的 Meta Space 标题"
+          rules={[{ required: true }]}
+        />
+        <ProFormText
+          width="md"
+          name="subtitle"
+          label="副标题"
+          placeholder="你的 Meta Space 副标题"
+          rules={[{ required: true }]}
+        />
+        <ProFormText
+          width="md"
+          name="author"
+          label="作者"
+          placeholder="作为 Meta Space 拥有者的名称"
+          rules={[{ required: true }]}
+        />
+        <ProFormTextArea
+          width="md"
+          name="description"
+          label="描述"
+          placeholder="你的 Meta Space 描述"
+          rules={[{ required: true }]}
+        />
+        <ProFormSelect
+          mode="tags"
+          width="md"
+          name="keywords"
+          fieldProps={{
+            open: false,
+          }}
+          label="关键字"
+          extra="其他人在搜索这些关键字时会更容易找到你的站点"
+          rules={[{ required: true }]}
+        />
+        <ProFormSelect
+          width="md"
+          name="language"
+          label="语言"
+          rules={[{ required: true, message: '请选择您的站点语言' }]}
+          valueEnum={{
+            zh: '中文',
+            en: 'English',
+            jp: '日本語',
+            es: 'Español',
+          }}
+        />
+        <ProFormText
+          width="md"
+          name="timezone"
+          label="时区"
+          placeholder="Meta Space 时区设置"
+          rules={[{ required: true }]}
+        />
+        <ProForm.Item
+          name="favicon"
+          getValueProps={(value) => [value]}
+          valuePropName={'fileList'}
+          label="网站图标"
+          extra=".ico格式，展示在标签页上，可用工具从图片生成"
+          rules={[{ required: true, message: '请上传一个站点图标' }]}
         >
-          <ProFormText
-            width="md"
-            name="title"
-            label="标题"
-            placeholder="你的 Meta Space 标题"
-            rules={[{ required: true }]}
-          />
-          <ProFormText
-            width="md"
-            name="subtitle"
-            label="副标题"
-            placeholder="你的 Meta Space 副标题"
-            rules={[{ required: true }]}
-          />
-          <ProFormText
-            width="md"
-            name="author"
-            label="作者"
-            placeholder="作为 Meta Space 拥有者的名称"
-            rules={[{ required: true }]}
-          />
-          <ProFormTextArea
-            width="md"
-            name="description"
-            label="描述"
-            placeholder="你的 Meta Space 描述"
-            rules={[{ required: true }]}
-          />
-          <ProFormSelect
-            mode="tags"
-            width="md"
-            name="keywords"
-            fieldProps={{
-              open: false,
-            }}
-            label="关键字"
-            extra="其他人在搜索这些关键字时会更容易找到你的站点"
-            rules={[{ required: true }]}
-          />
-          <ProFormSelect
-            width="md"
-            name="language"
-            label="语言"
-            rules={[{ required: true, message: '请选择您的站点语言' }]}
-            valueEnum={{
-              zh: '中文',
-              en: 'English',
-              jp: '日本語',
-              es: 'Español',
-            }}
-          />
-          <ProFormText
-            width="md"
-            name="timezone"
-            label="时区"
-            placeholder="Meta Space 时区设置"
-            rules={[{ required: true }]}
-          />
-          <ProForm.Item
-            name="favicon"
-            getValueProps={(value) => [value]}
-            valuePropName={'fileList'}
-            label="网站图标"
-            extra=".ico格式，展示在标签页上，可用工具从图片生成"
-            rules={[{ required: true, message: '请上传一个站点图标' }]}
-          >
-            <Upload
-              title="上传站点图标"
-              listType="picture-card"
-              /*
+          <Upload
+            title="上传站点图标"
+            listType="picture-card"
+            /*
               // @ts-ignore */
-              customRequest={customRequest}
-              showUploadList={false}
-            >
-              {imageUrl ? (
-                <img src={imageUrl} alt="favicon preview" style={{ width: '80%', height: '80%' }} />
-              ) : (
-                uploadButton
-              )}
-            </Upload>
-          </ProForm.Item>
-        </ProForm>
-      </ProCard>
+            customRequest={customRequest}
+            showUploadList={false}
+          >
+            {imageUrl ? (
+              <img src={imageUrl} alt="favicon preview" style={{ width: '80%', height: '80%' }} />
+            ) : (
+              uploadButton
+            )}
+          </Upload>
+        </ProForm.Item>
+      </ProForm>
     </div>
   );
 };
