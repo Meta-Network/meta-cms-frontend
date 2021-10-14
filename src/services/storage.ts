7becaa6b67b4c07158feb0ab34287ff9ce966ac9
xiaotiandada <istianlei@qq.com>
2021-10-14 23:09:50 +0800

    feat: modify url

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,5 +1,7 @@
 /**
  * storage
  * any file
+ * copy global.ts META_STORAGE_API
  */
-export const StorageFleek = `https://fleek-storage.vercel.mttk.net/fleek/storage`;
+export const StorageFleek =
+  META_STORAGE_API || 'https://fleek-storage.vercel.mttk.net/fleek/storage';
