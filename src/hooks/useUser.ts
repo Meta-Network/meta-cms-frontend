01681fff61736d52ffa6e9cc29b923b39734afb7
xiaotiandada <istianlei@qq.com>
2022-01-14 16:44:31 +0800

    feat: :ant: update user info

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,25 @@
+import { queryCurrentUser } from '@/services/api/meta-ucenter';
+import { useCallback } from 'react';
+import { useModel } from 'umi';
+
+export default function useUser() {
+  const { setInitialState } = useModel('@@initialState');
+
+  /**
+   * refresh User Info
+   * 仅重新获取用户信息
+   */
+  const refreshUserInfo = useCallback(async () => {
+    const currentUserResult = await queryCurrentUser();
+    if (currentUserResult.statusCode === 200) {
+      await setInitialState((s) => ({
+        ...s,
+        currentUser: currentUserResult.data,
+      }));
+    }
+  }, [setInitialState]);
+
+  return {
+    refreshUserInfo,
+  };
+}
--- src/pages/user/Information/index.tsx
+++ src/pages/user/Information/index.tsx
@@ -1,7 +1,7 @@
 import { uploadFileToIpfs } from '@/services/api/global';
 import { useModel, useIntl } from 'umi';
 import ImgCrop from 'antd-img-crop';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Button, Upload, message, Card } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import { PageContainer } from '@ant-design/pro-layout';
@@ -9,33 +9,57 @@ import FormattedDescription from '@/components/FormattedDescription';
 import { updateUserInfo } from '@/services/api/meta-ucenter';
 import ProForm, { ProFormText, ProFormTextArea } from '@ant-design/pro-form';
 import styles from './index.less';
+import { rules } from '../../../../config/index';
+import useUser from '@/hooks/useUser';
 
 const BaseView: React.FC = () => {
   const intl = useIntl();
   const { initialState } = useModel('@@initialState');
+  const { refreshUserInfo } = useUser();
   const [userAvatar, setUserAvatar] = useState(initialState?.currentUser?.avatar);
+  const [updateUserAvatarLoading, setUpdateUserAvatarLoading] = useState<boolean>(false);
+  const [updateUserInfoLoading, setUpdateUserInfoLoading] = useState<boolean>(false);
 
-  useEffect(() => {
-    updateUserInfo({ avatar: userAvatar }).then(() => '');
-  }, [userAvatar]);
-
+  /**
+   * upload Image
+   * 因为特性 如果上传过一次的图片使用裁剪是无效的
+   * @param param0
+   */
   const uploadImageRequest = async ({ file }: { file: File }) => {
-    // TODO: Need i18n here
     const done = message.loading('上传图片中...请稍候', 0);
+    setUpdateUserAvatarLoading(true);
     const result = await uploadFileToIpfs(file);
     done();
+    setUpdateUserAvatarLoading(false);
 
     if (result?.statusCode === 201) {
-      message.success('图片上传成功。');
-      setUserAvatar(result.data.publicUrl);
+      const url = result.data.publicUrl;
+      const updateUserAvatarResult = await updateUserInfo({ avatar: url });
+      if (updateUserAvatarResult.statusCode === 200) {
+        message.success('头像更新成功');
+        setUserAvatar(url);
+        refreshUserInfo();
+      } else {
+        message.error('头像更新失败');
+      }
     } else {
       message.error('图片上传失败。');
     }
   };
 
   const handleFinish = async (values: GLOBAL.UserInfo) => {
-    await updateUserInfo(values);
-    message.success(intl.formatMessage({ id: 'messages.profile.updateSuccess' }));
+    const done = message.loading('更新用户信息...请稍候', 0);
+    setUpdateUserInfoLoading(true);
+    const updateUserInfoResult = await updateUserInfo(values);
+    done();
+    setUpdateUserInfoLoading(false);
+
+    if (updateUserInfoResult.statusCode === 200) {
+      message.success(intl.formatMessage({ id: 'messages.profile.updateSuccess' }));
+      refreshUserInfo();
+    } else {
+      message.success('更新失败');
+    }
   };
 
   const AvatarView = ({ currentAvatar }: { currentAvatar: string }) => (
@@ -50,7 +74,7 @@ const BaseView: React.FC = () => {
         {/* @ts-ignore */}
         <Upload customRequest={uploadImageRequest} showUploadList={false}>
           <div className={styles.buttonView}>
-            <Button>
+            <Button loading={updateUserAvatarLoading}>
               <UploadOutlined />
               {intl.formatMessage({ id: 'messages.profile.changeAvatar' })}
             </Button>
@@ -78,15 +102,24 @@ const BaseView: React.FC = () => {
                     display: 'none',
                   },
                 },
-                submitButtonProps: {
-                  children: intl.formatMessage({ id: 'messages.profile.updateBaseInfo' }),
+                submitButtonProps: {},
+                render: () => {
+                  return [
+                    <Button
+                      type="primary"
+                      htmlType="submit"
+                      key="submit"
+                      loading={updateUserInfoLoading}
+                    >
+                      更新
+                    </Button>,
+                  ];
                 },
               }}
               initialValues={{ ...initialState?.currentUser }}
               hideRequiredMark
             >
               <ProFormText
-                width="md"
                 name="nickname"
                 label={intl.formatMessage({ id: 'messages.profile.form.nickname' })}
                 rules={[
@@ -95,6 +128,11 @@ const BaseView: React.FC = () => {
                     message: intl.formatMessage({
                       id: 'messages.profile.form.pleaseEnterNickname',
                     }),
+                  },
+                  {
+                    min: rules.nickname.min,
+                    max: rules.nickname.max,
+                    message: '长度 {{ min }}-{{ max }}',
                   },
                 ]}
               />
@@ -106,6 +144,7 @@ const BaseView: React.FC = () => {
                     required: true,
                     message: intl.formatMessage({ id: 'messages.profile.form.pleaseEnterBio' }),
                   },
+                  { min: rules.bio.min, max: rules.bio.max, message: '长度 {{ min }}-{{ max }}' },
                 ]}
                 placeholder={intl.formatMessage({ id: 'messages.profile.form.bio' })}
               />
